482bf74131c8d078681c99bd3b4305290fed3155
Leandro Soriano Sawczuk <leandrosorianos@gmail.com>
2025-04-22 12:18:40 -0400

    Implementar lazy loading y probar performance con Google Lighthouse

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Módulo 5 - Despliegue y Optimización de Aplicaciones Web
 - Firebase para desplegar proyecto Angular.
 - Ramas para desarrollo y testing.
+- Lazy Loading and testing con Google Lighthouse.
--- proyecto-firebase/src/app/app.component.ts
+++ proyecto-firebase/src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
